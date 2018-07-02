5fe57ccf14b729836124f4b693b8b1e6246f50b2
alyrow <36855289+alyrow@users.noreply.github.com>
2018-07-02 17:12:39 +0200

    Sn

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ console.log('listening on *:'+process.env.PORT);
 io.on('connection', function(socket){
  console.log("User Online ("+socket.request.connection.remoteAddress+")\n");
 
+
+
+socket.on('add user', (username) => { 
+ // we store the username in the socket session for this client 
+socket.username = username; 
+socket.broadcast.emit('user joined', { 
+username: socket.username
+}); 
+});
+
  socket.on('message', function(data){
   if (data == '') { return false; }
   io.emit('message', data);
